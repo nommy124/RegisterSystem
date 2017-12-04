009271963a33a5b3f12f4ee66703aa0ab49508a8
nommy124 <33672360+nommy124@users.noreply.github.com>
2017-12-04 16:47:30 -0500

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,57 @@
+#     item_num  name  price  stock sold
+
+items = { 1: ["Milk", 3.00, 100, 0],
+          2: ["Eggs", 2.50, 100, 0],
+          3: ["Bread",2.00, 100, 0],
+          4: ["Tea", 1.50, 100, 0],
+          5: ["Chickpeas", 0.50, 100, 0],
+          6: ["Juice", 5.00, 100, 0],
+          7: ["Lotion", 9.00, 100, 0],
+          8: ["Advil", 4.00, 100, 0],
+          9: ["Pancake", 3.00, 100, 0],
+          10: ["Wine", 15.00, 100, 0],
+          11: ["Muffin", 7.25, 100, 0],
+          12: ["Shampoo", 9.00, 100, 0],
+          13: ["Chips", 3.00, 100, 0],
+          14: ["Yogurt", 6.00, 100, 0],
+          15: ["Coffee", 2.00, 100, 0],
+          16: ["Pasta", 4.35, 100, 0],
+          17: ["Chicken", 5.60, 100, 0],
+          18: ["Cheese", 3.50, 100, 0],
+          19: ["Ketchup", 4.15, 100, 0],
+          20: ["Cookies", 3.00, 100, 0],
+         
+
+}
+
+#         num   name   rate  stock  sold  
+produce = { 1: ["Apple", 1.50, 100, 0],
+            2: ["Orange", 2.00, 100, 0],
+            3: ["Banana", 0.50, 100, 0],
+            4: ["Avocado", 2.50, 100, 0],
+            5: ["Grapefruit", 1.50, 100, 0],
+            6: ["Yam", 2.00, 100, 0],
+            7: ["Cilantro", .50, 100, 0],
+            8: ["Pear", 1.75, 100, 0],
+            9: ["Peach", 2.00, 100, 0],
+            10: ["Watermelon", 2.25, 100, 0],
+            11: ["Grape", 1.50, 100, 0],
+            12: ["Cherry", 1.60, 100, 0],
+            13: ["Tomato", 2.30, 100, 0],
+            14: ["Potato", 2.50, 100, 0],
+            15: ["Lemon", 1.50, 100, 0],
+            16: ["Lime", 0.70, 100, 0],
+
+
+    }
+
+
+# examples - shauib review this please to understand
+
+print (items[1][1]) # prints only price
+print (items[10][0],items[10][1]) # prints both item and price "wine 15.00"
+
+items[1][3] += 1
+items[1][2] -= 1
+print (items [1][0], items[1][2], items[1][3]) # prints newly updated 'milk 99 1'
+
