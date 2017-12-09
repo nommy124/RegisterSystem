2ef742e3eb668fe24c19099d364b93471f5dbd38
shuaibahmed26 <33672386+shuaibahmed26@users.noreply.github.com>
2017-12-09 15:53:22 -0500

    Add files via upload

---
+++ instructions.py
@@ -0,0 +1,30 @@
+def instructions():
+    print
+    (
+    """
+    Thank you for using the Grocery Shopping Calculator. To login use one of the
+    following username/password combinations.
+    1. Username: nouman21 / Password: 4567
+    2. Username: khawaja26 / Password: 1234
+
+    The program allows users to operate a simulated Point of Sale (POS) system
+    to conduct transactions. The option available in this application include:
+    Sales, Refunds, Reports, Inventory, and the option to Exit the application.
+
+    Sales: Input any item number between 0-34. User can enter the quantity of
+    items being purchasedContinue entering items until you are done. Enter -1
+    to finish and display a receipt.
+
+    Refunds: Input the corresponding item number between 0-34 that you would
+    like a refund for. Enter the quantity of items being returned.
+
+    Reports: Input option 3 to use the reports function. Main purpose of
+    this function is to display reports of the total sales figures. 
+
+    Inventory: Inputting option 4 displays the total inventory list and the
+    corresponding item number, price, and quantity available.
+
+    Exit: Input 5 on main menu screen to exit the application. 
+
+    """
+    )
